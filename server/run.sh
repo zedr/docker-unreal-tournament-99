9b4f64a22c794f24b879a6cbef4134446a791dea
Rigel Di Scala <zedr@zedr.com>
2017-06-03 02:17:32 +0100

    Extend the open ports for the server

--- server/run.sh
+++ server/run.sh
@@ -5,7 +5,7 @@ COMMAND=/bin/bash
 docker run \
     --rm \
     --privileged \
-    -p 7777-7778:7777-7778/udp \
+    -p 7777-7781:7777-7781/udp \
     -p 27900:27900/udp \
     -e "DISPLAY=unix:0.0" \
     -v="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
